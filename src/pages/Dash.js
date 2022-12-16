9a667d596310cc432181227b12530bba8695dffd
chichi <fateehahasnain@gmail.com>
2022-12-16 12:36:01 -0500

    add navbar and animations

--- src/pages/Dash.js
+++ src/pages/Dash.js
@@ -1,13 +1,15 @@
 import { Link } from 'react-router-dom'
-import Logo from '../components/Logo.js'
-import style from '../css/Login.module.css'
+import style from '../css/Dash.module.css'
 import React from 'react'
 
 
 const Dash = () => {
     return (
-        <div>
-        </div>
+        <>
+
+        
+        
+        </>
     )
 }
 
--- src/pages/TopBarLayout.js
+++ src/pages/TopBarLayout.js
@@ -3,31 +3,50 @@ import Logo from '../components/Logo'
 import { Link } from 'react-router-dom'
 import profile from '../img/profile.png'
 import styles from '../css/TopBar.module.css'
+import React from 'react'
 
 
 const TopBarLayout = () => {
 
+    const [down, setDown] = React.useState(false);
+
+
+    function Dropdown(){
+        return (
+            <ul className={styles.dropdown}>
+                <li> <Link className={styles.subnav} to="/profile"> Update Profile </Link> </li>
+                <li> <Link className={styles.subnav} to="/explore"> Log Out </Link> </li>
+                <li> <Link className={styles.subnav} to="/settings"> Settings </Link> </li>
+
+            </ul>
+        )
+    }
 
     return (
         <>
             <nav className={styles.topbar}>
                 <Logo size="small" />
-                <form>
+                <form className={styles.form}>
                     <input className={styles.searchbar} 
                         placeholder="I want to make.."/>
                 </form>
 
-                <div className={styles.navlinks}>
-                        <Link className={styles.links} to="/explore"> Explore </Link>
-                        <Link className={styles.links} to="/user"> My Recipes </Link>
-                        <Link className={styles.profilelink} to="/user"> <img className={styles.profile} src={profile}/>  </Link>
-                </div>
-                <nav className={styles.dropdowninvisible}>
-                    <Link>Update Profile</Link>
-                    <Link>Log Out</Link>
-                    <Link>Settings</Link>
-                </nav>
+                <ul className={styles.navlinks}>
+                    <li> <Link className={styles.links} to="/explore"> Explore </Link> </li>
+                    <li> <Link className={styles.links} to="/user"> My Recipes </Link> </li>
+                    <li className={styles.hasdrop} 
+                        onMouseEnter={() => setDown(true)} 
+                        onMouseLeave={() => setDown(false)}> 
+                        <Link className={styles.profilelink} to="/user"> 
+                            <img className={down ? styles.profilehov : styles.profile} src={profile} />
+                        </Link> 
+                        {down && <Dropdown/>}
+
+
+                    </li>
+                </ul>
             </nav>
+
             <Outlet />
         </>
     )
